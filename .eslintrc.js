a0fd7388f736c977ad51a7e0c0eb7273af6a1b8b
kiselev <georgiy.kiselev@loginom.ru>
2023-11-28 11:00:08 +0300

    added some eslint rules, prettier and vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,11 +22,96 @@ module.exports = {
     'node/no-missing-require': 'off',
     'node/shebang': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
-    quotes: ['warn', 'single', { avoidEscape: true }],
+    quotes: ['warn', 'double', { avoidEscape: true }],
     'node/no-unpublished-import': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/restrict-template-expressions": [
+      "warn",
+      {
+        "allowBoolean": true,
+        "allowNumber": true
+      }
+    ],
+    "@typescript-eslint/no-inferrable-types": [
+      "warn",
+      {
+        "ignoreParameters": true,
+        "ignoreProperties": true
+      }
+    ],
+    "no-sparse-arrays": [
+      "warn"
+    ],
+    "no-useless-escape": [
+      "warn"
+    ],
+    "no-nested-ternary": "error",
+    "no-trailing-spaces": "error",
+    "no-magic-numbers": "off",
+    "@typescript-eslint/no-magic-numbers": [
+      "warn",
+      {
+        "ignoreDefaultValues": true,
+        "ignoreArrayIndexes": true,
+        "ignoreNumericLiteralTypes": true,
+        "ignoreEnums": true,
+        "ignore": [
+          1,
+          0
+        ]
+      }
+    ],
+    "default-case": "warn",
+    "default-case-last": "warn",
+    "prefer-const": "error",
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      // ? https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/naming-convention.md
+      {
+        "selector": [
+          "interface"
+        ],
+        "prefix": [
+          "I"
+        ],
+        "format": [
+          "PascalCase"
+        ]
+      },
+      {
+        "selector": [
+          "typeAlias"
+        ],
+        "prefix": [
+          "T"
+        ],
+        "format": [
+          "PascalCase"
+        ]
+      },
+      {
+        "selector": [
+          "default"
+        ],
+        "format": [
+          "PascalCase",
+          "camelCase"
+        ]
+      },
+      {
+        "selector": [
+          "variableLike",
+          "memberLike",
+          "property"
+        ],
+        "format": [
+          "PascalCase",
+          "camelCase"
+        ]
+      }
+    ],
   },
 };
