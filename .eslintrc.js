84e89c35339cc82a961fb59ddcea561bd9735821
kiselev <georgiy.kiselev@loginom.ru>
2023-11-28 16:23:49 +0300

    fix: minor eslint add

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/restrict-template-expressions": [
       "warn",
       {
