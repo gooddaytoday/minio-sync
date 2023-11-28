e5471fbf2badf7c3d8f4313045fb4a9ed60cc606
kiselev <georgiy.kiselev@loginom.ru>
2023-11-28 16:36:55 +0300

    Added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import MinIO, { IMinIOConfig } from "./minio";
+import { Log } from "./utils";
+import { Watcher } from "./watcher";
+
+interface ISyncConfig {
+    MinIO: IMinIOConfig;
+}
+
+export default async function Sync(
+    rootPath: string,
+    config: ISyncConfig
+): Promise<Watcher> {
+    Log(`\n\n=== Syncing ${rootPath} with MinIO ===\n`);
+    const minIO = new MinIO(config.MinIO);
+    await minIO.Init().catch(e => {
+        console.error("Error while initializing minio", e);
+        throw e;
+    });
+    const watcher = new Watcher(rootPath, minIO);
+    return watcher;
+}
