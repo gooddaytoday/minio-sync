15d8304c199dac3f335a22875c45e243768daf82
kiselev <georgiy.kiselev@loginom.ru>
2023-11-30 17:30:40 +0300

    Queuing file operations

--- src/manager.ts
+++ src/manager.ts
@@ -1,4 +1,5 @@
 import { exists, stat } from "fs-extra";
+import queue from "p-queue";
 import * as path from "path";
 import { CalcEtag, Log } from "./utils";
 
@@ -26,6 +27,7 @@ export class Manager {
     private rootPath: string;
     private storage: IStorage;
     private permissions: IPermissions;
+    private queue = new queue({ concurrency: 1 });
 
     constructor(
         rootPath: string,
@@ -41,7 +43,9 @@ export class Manager {
         if (!this.permissions.Write) {
             Log(`UploadFile ${filePath}: Write permission denied`);
         } else {
-            void this.storage.UploadFile(objectName, filePath);
+            void this.queue.add(() =>
+                this.storage.UploadFile(objectName, filePath)
+            );
         }
     }
 
@@ -49,7 +53,9 @@ export class Manager {
         if (!this.permissions.Write) {
             Log(`UpdateFile ${filePath}: Write permission denied`);
         } else {
-            void this.storage.UpdateFile(objectName, filePath);
+            void this.queue.add(() =>
+                this.storage.UpdateFile(objectName, filePath)
+            );
         }
     }
 
@@ -57,43 +63,47 @@ export class Manager {
         if (!this.permissions.Write) {
             Log(`DeleteFile ${objectName}: Write permission denied`);
         } else {
-            void this.storage.DeleteFile(objectName);
+            void this.queue.add(() => this.storage.DeleteFile(objectName));
         }
     }
 
     public Sync(): void {
-        (async () => {
-            try {
-                Log(" --- SYNC ---");
-                if (!this.permissions.Read) {
-                    Log("Sync: Read permission denied");
-                } else {
-                    const objects = this.storage.Objects;
-                    const downloads: Promise<void>[] = [];
-                    for (const [obj, objData] of objects) {
-                        const fullpath = path.join(this.rootPath, obj);
-                        if (await exists(fullpath)) {
-                            const fileStat = await stat(fullpath);
-                            if (
-                                (objData.size == 0 && fileStat.size == 0) ||
-                                (objData.size == fileStat.size &&
-                                    objData.etag === (await CalcEtag(fullpath)))
-                            ) {
-                                continue;
-                            }
-                        }
-                        downloads.push(
-                            this.storage.DownloadFile(obj, fullpath)
-                        );
-                    }
-                    await Promise.all(downloads);
+        if (!this.permissions.Read) {
+            Log("Sync: Read permission denied");
+            return;
+        }
+        this.queue
+            .add(async () => {
+                try {
+                    Log(" --- SYNC ---");
+                    await this.DownloadObjects();
+                } finally {
+                    Log(" --- SYNC ENDED ---");
                 }
-            } finally {
-                Log(" --- SYNC ENDED ---");
+            })
+            .catch(e => {
+                console.error("Error while syncing", e);
+                throw e;
+            });
+    }
+
+    private async DownloadObjects(): Promise<void> {
+        const objects = this.storage.Objects;
+        const downloads: Promise<void>[] = [];
+        for (const [obj, objData] of objects) {
+            const fullpath = path.join(this.rootPath, obj);
+            if (await exists(fullpath)) {
+                const fileStat = await stat(fullpath);
+                if (
+                    (objData.size == 0 && fileStat.size == 0) ||
+                    (objData.size == fileStat.size &&
+                        objData.etag === (await CalcEtag(fullpath)))
+                ) {
+                    continue;
+                }
             }
-        })().catch(e => {
-            console.error("Error while syncing", e);
-            throw e;
-        });
+            downloads.push(this.storage.DownloadFile(obj, fullpath));
+        }
+        await Promise.all(downloads);
     }
 }
