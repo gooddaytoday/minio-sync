6056f102ff7de0f7e08123ffde50cb4de6db5a5c
kiselev <georgiy.kiselev@loginom.ru>
2024-06-11 14:13:52 +0300

    feat: simplified queueing
    1. Global queue doesn't affect named queues
    2. Named queues executes in global if global gueue is not empty

--- src/queueing.ts
+++ src/queueing.ts
@@ -1,69 +1,57 @@
 import queue from "p-queue";
-import { DebugFail, GUID } from "./utils";
 
-export default class Queueing {
-    private globalQueue: queue = new queue({ concurrency: 1 });
-    private queueMap = new Map<string, queue>();
-    private queueMapActiveCount: number = 0;
+export default class Queue {
+    private globalQueue: queue;
+    private namedQueues: Map<string, queue>;
+    private namedQueuesActiveCount: number = 0;
 
-    /** Adds cb to global queue that run exclusive after all in queueMap and non-concurrent with new in queueMap */
-    public AddToGlobalQueue(cb: () => Promise<void>): void {
-        let task: Promise<void> | undefined;
-        if (this.queueMap.size > 0) {
-            const allQueues = Array.from(this.queueMap.values()).map(q =>
-                q.onIdle()
-            );
-            const newGlobalTask = Promise.all(allQueues).then(cb);
-            task = this.globalQueue.add(() => newGlobalTask);
-        } else {
-            task = this.globalQueue.add(cb);
-        }
-        task.catch(e => {
+    constructor() {
+        this.globalQueue = new queue({ concurrency: 1 });
+        this.namedQueues = new Map();
+    }
+
+    public AddToGlobalQueue(task: () => Promise<void>): void {
+        this.globalQueue.add(task).catch(e => {
             console.error("Error in global queue", e);
             throw e;
         });
     }
 
-    /** Adds cb to queue for objectName. If globalQueue is not empty, cb will run after all in queueMap */
-    public AddToQueue(objectName: string, cb: () => Promise<void>): void {
-        let objQueue = this.queueMap.get(objectName);
-        if (this.globalQueue.size > 0 || this.globalQueue.pending > 0) {
-            const newTask = this.globalQueue.onIdle().then(cb);
-            cb = () => newTask;
-
-            if (objQueue && (objQueue.size > 0 || objQueue.pending > 0)) {
-                // Rename queue to avoid endless global queue if there was task for this object before global queue
-                this.RenameQueue(objectName, objQueue);
-                objQueue = undefined; // Make it undefined to create new queue
-            }
+    /** Runs the given task in a named queue. If the global queue isn't empty, the task will be added to the global queue instead. */
+    public AddToQueue(objectName: string, task: () => Promise<void>): void {
+        if (!this.namedQueues.has(objectName)) {
+            this.namedQueues.set(objectName, new queue({ concurrency: 1 }));
         }
 
-        if (!objQueue) {
-            objQueue = new queue({ concurrency: 1 });
-            this.queueMap.set(objectName, objQueue);
-        }
-        objQueue.add(cb).catch(e => {
-            console.error(`Error in queue of object ${objectName}`, e);
-            throw e;
-        });
+        const namedQueue = this.namedQueues.get(objectName)!;
 
-        this.queueMapActiveCount++;
-        void objQueue.onIdle().finally(() => {
-            if (--this.queueMapActiveCount == 0) {
-                for (const [key, value] of this.queueMap) {
-                    if (value.size == 0 && value.pending == 0) {
-                        this.queueMap.delete(key);
-                    } else {
-                        DebugFail(`Queue for ${key} should be empty`);
-                    }
-                }
+        const wrappedTask = async () => {
+            if (this.globalQueue.size === 0 && this.globalQueue.pending === 0) {
+                await task();
+            } else {
+                await this.globalQueue.add(() => task());
             }
-        });
-    }
+        };
 
-    private RenameQueue(objectName: string, queue: queue) {
-        this.queueMap.delete(objectName); // Remove from map to create new
-        const newObjectName = `${objectName}_${GUID()}`;
-        this.queueMap.set(newObjectName, queue);
+        this.namedQueuesActiveCount++;
+        namedQueue
+            .add(wrappedTask)
+            .catch(e => {
+                console.error(
+                    `Error in named queue for object ${objectName}`,
+                    e
+                );
+                throw e;
+            })
+            .finally(() => {
+                this.namedQueuesActiveCount--;
+            });
+
+        // Remove namedQueue from this.namedQueues when queue become empty
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        namedQueue.onIdle().finally(() => {
+            if (namedQueue.size === 0 && namedQueue.pending === 0)
+                this.namedQueues.delete(objectName);
+        });
     }
 }
